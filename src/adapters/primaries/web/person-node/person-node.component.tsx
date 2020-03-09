2a22cfac72625a0ff2d4b81a45370f6794bede44
Georges Cosson <georges.cosson@phantombuster.com>
2020-03-09 07:16:22 +0100

    style(personNode): updated person renderer

--- src/adapters/primaries/web/person-node/person-node.component.tsx
+++ src/adapters/primaries/web/person-node/person-node.component.tsx
@@ -10,9 +10,11 @@ const PersonNode = ({ person }: IPersonNodeProps) => {
 		return null
 	}
 
+	// https://stackoverflow.com/questions/31110184/react-synthetic-event-distinguish-left-and-right-click-events
+
 	return (
-		<div className="person-node d-flex w-100 h-100 rounded-circle" style={{backgroundColor: "pink" }}>
-			<span className="align-self-center mx-auto">{person.name}</span>
+		<div className="person-node d-flex w-100 h-100 rounded-circle bg-dark">
+			<span className="align-self-center mx-auto font-weight-bold">{person.name}</span>
 		</div>
 	)
 }
